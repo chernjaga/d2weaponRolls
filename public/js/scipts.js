51386de3634a492e8722904ec98ec7b6aa8d5711
Aliaksandr.Limasau <Aliaksandr.Limasau@bitbucket.wolterskluwer.io>
2019-04-10 13:10:05 +0300

    html 5 mode on

--- public/js/scipts.js
+++ public/js/scipts.js
@@ -393,7 +393,7 @@ angular.module('d2RollsApp', ['ui.router'])
     $stateProvider.state(weaponListState);
     $stateProvider.state(weaponViewState);
     $urlRouterProvider.otherwise('/weaponList/en');
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 });
 angular.module('d2RollsApp').factory('fetchManifestService', ['$http', function($http) {
     var weaponListArray = [];
